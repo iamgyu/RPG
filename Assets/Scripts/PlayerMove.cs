f794165f6816a9a89ca687f99d34d7dee73870fc
DESKTOP-7R096V6\iamgy <iamgyu@naver.com>
2017-05-13 17:12:26 +0900

    RPG 프로젝트 시작 koko 메카님 추가 마커 추가 지형 추가

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 4.0f;
+    public float turnSpeed = 360.0f;
+    public float gravityRatio = 0.5f;
+
+    CharacterController cc;
+
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, turnSpeed * h * Time.deltaTime, 0);
+        cc.Move((transform.forward * v * moveSpeed + Physics.gravity * gravityRatio) * Time.deltaTime);
+    }
+}
